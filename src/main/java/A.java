15a036b8078e5d84c1641af8fc1114d91933851e
Renat I. Kashapov <renatka@mail.ru>
2020-10-09 23:26:35 +0300

    v6

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class A {
     public static void main(String ... args){
         System.out.println("Hello, gradle + git!");
+        List<NO> list = new ArrayList();
+        list.add(NO.ONE);
     }
 }
---
+++ src/main/java/NO.java
@@ -0,0 +1,3 @@
+public enum NO {
+    ONE
+}
