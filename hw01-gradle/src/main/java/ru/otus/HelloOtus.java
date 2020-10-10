99b1adb5acd318efaff9924eac99b65ea5620a83
Renat I. Kashapov <renatka@mail.ru>
2020-10-11 00:42:18 +0300

    v11. To check.

---
+++ hw01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -0,0 +1,10 @@
+package ru.otus;
+
+import com.google.common.math.IntMath;
+
+public class HelloOtus {
+    public static void main(String ... args) {
+        System.out.println("Hello, Otus!");
+        System.out.println("Hello, Guava!: IntMath.factorial(7) = " + IntMath.factorial(7));
+    }
+}
