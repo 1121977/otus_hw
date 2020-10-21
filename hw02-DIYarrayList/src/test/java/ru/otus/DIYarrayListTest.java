f807557d962a5c8a1a9953781deb7ce8744eab5a
Renat I. Kashapov <renatka@mail.ru>
2020-10-22 01:03:58 +0300

    V.13 Tests were added.

--- hw02-DIYarrayList/src/test/java/ru/otus/DIYarrayListTest.java
+++ hw02-DIYarrayList/src/test/java/ru/otus/DIYarrayListTest.java
@@ -1,17 +1,25 @@
 package ru.otus;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Collections;
 
 class DIYarrayListTest {
 
+    private DIYarrayList<Integer> diYarrayList;
+
     @BeforeEach
     void setUp() {
+        diYarrayList = new DIYarrayList<>();
     }
 
     @Test
-    void add() {
+    void addTest() {
+        boolean expectedAddAllFunctionResult = true;
+        boolean actualAddAllFunctionResult = Collections.addAll(diYarrayList, 1,2,3);
+
+        Assertions.assertEquals(expectedAddAllFunctionResult, actualAddAllFunctionResult);
     }
 }
