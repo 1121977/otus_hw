3fd8ba79d1a80beda7b3daa76d8309df37171f1a
Renat I. Kashapov <renatka@mail.ru>
2020-10-22 01:02:23 +0300

    V.13. Tests were added.

---
+++ hw02-DIYarrayList/src/test/java/ru/otus/DIYarrayListTest.java
@@ -0,0 +1,17 @@
+package ru.otus;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DIYarrayListTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void add() {
+    }
+}
