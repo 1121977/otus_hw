2589483e51591af1863060f81fe344b3f664734e
Renat I. Kashapov <renatka@mail.ru>
2020-10-26 22:10:41 +0300

    v12. Before refactoring.

--- hw03-testFW/src/main/java/ru/otus/TestFW.java
+++ hw03-testFW/src/main/java/ru/otus/TestFW.java
@@ -7,36 +7,22 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
 
 public class TestFW {
-    static short failedTest;
+    private static short failedTest;
     public static void main(String ... args){
         System.out.println("Hello, Test Framework");
         testStarter(TestedClass.class);
     }
     static void testStarter(Class<?> clazz) {
-        Method methods[] = clazz.getDeclaredMethods();
+        Method [] methods = clazz.getDeclaredMethods();
         List<Method> methodsWithAfterAnnotaion = new ArrayList<>();
         List<Method> methodsWithBeforeAnnotaion = new ArrayList<>();
         List<Method> methodsWithTestAnnotation = new ArrayList<>();
-        for(Method method:methods) {
-            Annotation[] annotations = method.getDeclaredAnnotations();
-            if (annotations.length!=0)
-                for (Annotation annotation:annotations)
-                    switch (annotation.toString()) {
-                        case ("@ru.otus.After()"):
-                            methodsWithAfterAnnotaion.add(method);
-                            break;
-                        case ("@ru.otus.Before()"):
-                            methodsWithBeforeAnnotaion.add(method);
-                            break;
-                        case ("@ru.otus.Test()"):
-                            methodsWithTestAnnotation.add(method);
-                            break;
-                        default:
-                            throw new AnnotationTypeMismatchException(method, "Unkown annotaion");
-                    }
+        for(Method method:methods){
+            if(method.isAnnotationPresent(Before.class)){ methodsWithBeforeAnnotaion.add(method);}
+            if(method.isAnnotationPresent(Test.class)){methodsWithTestAnnotation.add(method);}
+            if(method.isAnnotationPresent(After.class)){methodsWithAfterAnnotaion.add(method);}
         }
         methodsWithTestAnnotation.forEach(_methodTest -> {
             Object testedObject;
