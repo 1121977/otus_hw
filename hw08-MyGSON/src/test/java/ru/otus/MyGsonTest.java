4979e959e6d4437ec735e1b9f1e0a2f1754955fd
Renat I. Kashapov <renatka@mail.ru>
2021-01-09 15:46:04 +0300

    v14 Iteration 1.

--- hw08-MyGSON/src/test/java/ru/otus/MyGsonTest.java
+++ hw08-MyGSON/src/test/java/ru/otus/MyGsonTest.java
@@ -1,7 +1,6 @@
 package ru.otus;
 
 import com.google.gson.Gson;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,7 +11,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MyGsonTest {
 
-    SomeClass someObject = new SomeClass();
+    SomeClass someObject;
+    SomeClass anotherObject;
 
     @BeforeEach
     void setUp() {
@@ -20,20 +20,22 @@ class MyGsonTest {
 
     @Test
     void toJSONTest(){
-        List<String> list = new ArrayList<>();
+        SomeClass.Builder someClassBuilder = new SomeClass.Builder();
+        List<? super Object> list = new ArrayList<>();
+        list.add(Boolean.TRUE);
         for (int i = 1; i < 4; i++) { list.add(Integer.toString(i)); }
-        someObject.setA(5);
-        someObject.setB(list);
-        someObject.setC(new float[] {1F, 2F, 3F});
-        someObject.setD("Hello, Otus!");
-        someObject.setE(Math.E);
-        String jsonString;
+        someObject = someClassBuilder
+                .setA(5)
+                .setB(list)
+                .setC(new float[] {1F,2F,3F})
+                .setD("Hello, Otus!")
+                .setE(Math.E)
+                .setF((short) 27)
+                .build();
         Gson gson = new Gson();
-        jsonString = gson.toJson(someObject);
-        System.out.println(jsonString);
         MyGson myGson = new MyGson();
-        jsonString = myGson.toJson(someObject);
-        assertEquals(1l,1l);
+        anotherObject = gson.fromJson(gson.toJson(someObject), SomeClass.class);
+        assertEquals(someObject,anotherObject);
     }
 
 }
