79a5c8c140027bdbab26decc3d5b86ce27a27126
1121977 <m066ar16RUS>
2023-05-29 17:56:42 +0300

    Beta Demo

--- hw14-SpringMVC/src/main/resources/db/migration/V2__Add_clients.sql
+++
@@ -1,9 +0,0 @@
-insert into client (ID, NAME) values (1, 'Axel');
-insert into client (ID, NAME) values (2, 'Zorro');
-insert into client (ID, NAME) values (3, 'Tourovich');
-insert into phoneData (ID, NUMBER) values (1, '+1111');
-insert into phoneData (ID, NUMBER) values (2, '+22222');
-insert into phoneData (ID, NUMBER) values (3, '+33333');
-update phoneData set clientid=1 where id=1;
-update phoneData set clientid=2 where id=2;
-update phoneData set clientid=3 where id=3;
